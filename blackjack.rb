872522cb6f64cc7df6cb2551d9ae484b0bca5d6a
christopherwperry <christopherwperry@gmail.com>
2017-09-18 10:04:43 -0400

    Initial Commit

---
+++ blackjack.rb
@@ -0,0 +1,32 @@
+class Dealer
+  attr_reader :hit, :stand, :deck, :hand
+end
+
+# puts "TODO Implement the game of blackjack."
+#
+# Classes needed:
+# Card
+# Deck
+# Dealer
+# Player
+# Hand
+#
+# Dealer:
+# -deals cards
+# -deicded to hit or stand
+# -has a deck
+# -has a hand
+#
+# Player:
+# -decides to hit or stand
+# -has a hand
+#
+# Game:
+# -setup
+# -input
+# -output
+# -order of player
+#
+# Hand:
+# -calculate score
+# -has cards
